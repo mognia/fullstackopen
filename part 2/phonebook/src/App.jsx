b7dbb3d2589c6570e9299f4e95674699ee2bd091
Mognia <1mognia@gmail.com>
2026-01-06 02:11:10 +0300

    phonebook app finished

--- part 2/phonebook/src/App.jsx
+++ part 2/phonebook/src/App.jsx
@@ -1,34 +1,67 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import Filter from "./components/Filter.jsx";
 import PersonForm from "./components/PersonsForm.jsx";
 import Persons from "./components/Persons.jsx";
+import Notification from "./components/Notification.jsx";
+import personService from './services/persons'
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [filter, setFilter] = useState('')
-
+    const [infoMessage, setInfoMessage] = useState(null)
+    const [messageType, setMessageType] = useState('success')
+    useEffect(() => {
+        personService.getAll().then(initialPersons => {
+            setPersons(initialPersons)
+        })
+    }, [])
     const addName = (event) => {
         event.preventDefault()
-        const nameObject = {
-            name: newName,
-            number: newNumber,
+        const existingPerson = persons.find(p => p.name === newName)
+        if (existingPerson) {
+            if (window.confirm(`${newName} is already added, replace the old number with a new one?`)) {
+                const changedPerson = { ...existingPerson, number: newNumber }
+                personService
+                    .update(existingPerson.id, changedPerson)
+                    .then(returnedPerson => {
+                        setPersons(persons.map(p => p.id !== existingPerson.id ? p : returnedPerson))
+                        setNewName('')
+                        setNewNumber('')
+                        setMessageType('success')
+                        setInfoMessage(`Updated ${returnedPerson.name}'s number`)
+                        setTimeout(() => setInfoMessage(null), 5000)
+                    })
+                    .catch(() => {
+                        // Ex 2.17: Error Notification
+                        setMessageType('error')
+                        setInfoMessage(`Information of ${newName} has already been removed from server`)
+                        setTimeout(() => setInfoMessage(null), 5000)
+                        setPersons(persons.filter(p => p.id !== existingPerson.id))
+                    })
+            }
+            return
         }
-        const nameExists = persons.some(person => person.name === newName)
-        if (nameExists) {
-            alert(`${newName} is already added to numberbook`)
-        }else {
-            setPersons(persons.concat(nameObject))
-            setNewName('') // Resets the input field
+
+        const personObject = { name: newName, number: newNumber }
+        personService.create(personObject).then(returnedPerson => {
+            setPersons(persons.concat(returnedPerson))
+            setNewName('')
+            setNewNumber('')
+
+            setMessageType('success')
+            setInfoMessage(`Added ${returnedPerson.name}`)
+            setTimeout(() => setInfoMessage(null), 5000)
+        })
+    }
+    const deletePerson = (id, name) => {
+        if (window.confirm(`Delete ${name}?`)) {
+            personService.remove(id).then(() => {
+                setPersons(persons.filter(p => p.id !== id))
+            })
         }
     }
-
     const personsToShow = filter === ''
         ? persons
         : persons.filter(person =>
@@ -38,6 +71,7 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
+            <Notification message={infoMessage} type={messageType} />
             <Filter value={filter} onChange={(e) => setFilter(e.target.value)} />
 
             <h2>numberbook</h2>
@@ -49,7 +83,7 @@ const App = () => {
                 numberOnChange={(e) => setNewNumber(e.target.value)}
             />
             <h3>Numbers</h3>
-            <Persons personsToShow={personsToShow} />
+            <Persons personsToShow={personsToShow} deletePerson={deletePerson} />
         </div>
     )
 }
--- part 2/phonebook/src/components/Persons.jsx
+++ part 2/phonebook/src/components/Persons.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Persons = ({ personsToShow }) => (
+const Persons = ({ personsToShow, deletePerson }) => (
     <div>
         {personsToShow.map(person => (
             <p key={person.name}>
                 {person.name} {person.number}
+                <button onClick={() => deletePerson(person.id, person.name)}>delete</button>
             </p>
         ))}
     </div>
