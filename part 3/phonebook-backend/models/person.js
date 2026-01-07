1cba7ec2dfeefbf5c5b04d7444332647d640c2df
Mognia <1mognia@gmail.com>
2026-01-07 18:57:43 +0300

    remove duplicate Clean up the object returned by MongoDB

--- part 3/phonebook-backend/models/person.js
+++ part 3/phonebook-backend/models/person.js
@@ -32,13 +32,6 @@ const personSchema = new mongoose.Schema({
     }
   }
 })
-personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
 // Clean up the object returned by MongoDB
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
